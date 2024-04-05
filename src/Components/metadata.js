c0f96e62ed1be1c2ef3b1d902bf66c798c7b3274
Pratham <chudasamapratham142@gmail.com>
2024-04-05 19:40:54 +0530

    Update metadata images and dimensions

--- src/Components/metadata.js
+++ src/Components/metadata.js
@@ -32,31 +32,31 @@ export default function Metadata({
 			createMetaTag("og:url", window.location.href),
 			createMetaTag("og:description", description),
 			createMetaTag("og:type", "personal website"),
-			createMetaTag("og:image", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Fpratham-chudasama-co.png?alt=media&token=bd85143b-de76-4a69-b64d-669efe8541e0"),
-			createMetaTag("og:image:width", "1920"),
-			createMetaTag("og:image:height", "1080"),
+			createMetaTag("og:image", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Fog-long.png?alt=media&token=ad42aecb-3562-406b-9c0b-a9b25a698e78"),
+			createMetaTag("og:image:width", "1200"),
+			createMetaTag("og:image:height", "630"),
 
 			// twitter tags
 			createMetaTag("twitter:title", title),
 			createMetaTag("twitter:description", description),
 			createMetaTag("twitter:card", "summary_large_image"),
-			createMetaTag("twitter:image", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Fpratham-chudasama-co.png?alt=media&token=bd85143b-de76-4a69-b64d-669efe8541e0"),
-			createMetaTag("twitter:image:width", "1920"),
-			createMetaTag("twitter:image:height", "1080"),
+			createMetaTag("twitter:image", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Fog-long.png?alt=media&token=ad42aecb-3562-406b-9c0b-a9b25a698e78"),
+			createMetaTag("twitter:image:width", "1200"),
+			createMetaTag("twitter:image:height", "630"),
 
 			// favicon
-			createMetaTag("icon", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Fpratham-chudasama-co.png?alt=media&token=bd85143b-de76-4a69-b64d-669efe8541e0"),
+			createMetaTag("icon", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Fog-long.png?alt=media&token=ad42aecb-3562-406b-9c0b-a9b25a698e78"),
 
 			// mobile
 			createMetaTag("mobile-web-app-capable", "yes"),
 			createMetaTag("apple-mobile-web-app-capable", "yes"),
 			createMetaTag("apple-mobile-web-app-status-bar-style", "black"),
 			createMetaTag("apple-mobile-web-app-title", title),
-			createMetaTag("msapplication-TileImage", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Ffavicon.png?alt=media&token=8a6c5e1b-7f2f-4d4f-9d0b-1f9c5f9a8c2a"),
+			createMetaTag("msapplication-TileImage", "https://firebasestorage.googleapis.com/v0/b/personal-website-adcb1.appspot.com/o/Images%2Fog-long.png?alt=media&token=ad42aecb-3562-406b-9c0b-a9b25a698e78"),
 			createMetaTag("msapplication-TileColor", "#000000"),
 
 			// theme
-			createMetaTag("theme-color", "#000000"),
+			createMetaTag("theme-color", "#e2e2e5"),
 
 			// google
 			createMetaTag("google-site-verification", "253sXFl_IBcYz7SnU9bPOMsiYz9cIZjk0AnTsC1CjiU"),
